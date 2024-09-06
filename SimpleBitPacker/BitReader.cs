21e11b30f6adb02b3d342ad7d25d1f078fd7f071
ApexNite <128202315+ApexNite@users.noreply.github.com>
2024-09-05 20:18:40 -0500

    Add project files.

---
+++ SimpleBitPacker/BitReader.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace SimpleBitPacker {
+    public class BitReader {
+        private byte[] bytes;
+        private int offset = 0;
+
+        public BitReader(byte[] bytes) {
+            this.bytes = bytes;
+        }
+
+        public T Read<T>(int bits) where T : IBinaryInteger<T> {
+            T value = T.Zero;
+
+            for (int i = 0; i < bits; i++)
+                value |= (T)Convert.ChangeType(((bytes[offset / 8] & (1 << offset++ % 8)) != 0 ? 1UL : 0UL) << i, typeof(T));
+
+            return value;
+        }
+
+        public float ReadFloat(int bits) => BitConverter.ToSingle(BitConverter.GetBytes(Read<int>(bits)));
+
+        public bool ReadBool() => (bytes[offset / 8] & (1 << offset++ % 8)) != 0;
+    }
+}
---
+++ SimpleBitPacker/BitWriter.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace SimpleBitPacker {
+    public class BitWriter {
+        private List<byte> buffer = [0];
+        private int bitsInBuffer = 0;
+
+        public void Write<T>(T value, int bits) where T : IBinaryInteger<T> {
+            for (int i = 0; i < bits; i++)
+                WriteBit((value & (T.One << i)) != T.Zero);
+        }
+
+        public void Write(float value, int bits) => Write(BitConverter.ToInt32(BitConverter.GetBytes(value)), bits);
+
+        public void Write(bool value) => WriteBit(value);
+
+        public byte[] GetBytes() => buffer.ToArray();
+
+        private void WriteBit(bool bit) {
+            if (bitsInBuffer / 8 > buffer.Count - 1)
+                buffer.Add(0);
+
+            if (bit)
+                buffer[buffer.Count - 1] |= (byte)(1 << bitsInBuffer % 8);
+
+            bitsInBuffer++;
+        }
+    }
+}
