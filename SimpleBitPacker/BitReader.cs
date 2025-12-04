268287789ca0d5fcbecd37da7c3c0f3feb98eb41
ApexNite <128202315+ApexNite@users.noreply.github.com>
2025-12-04 11:04:26 -0600

    Formatting

--- SimpleBitPacker/BitReader.cs
+++ SimpleBitPacker/BitReader.cs
@@ -1,19 +1,14 @@
 ﻿using System.Numerics;
 
 namespace SimpleBitPacker {
-    public class BitReader {
-        private byte[] bytes;
-        private int offset = 0;
-
-        public BitReader(byte[] bytes) {
-            this.bytes = bytes;
-        }
+    public class BitReader(byte[] bytes) {
+        private int _offset = 0;
 
         public T Read<T>(int bits) where T : IBinaryInteger<T> {
             T value = T.Zero;
 
             for (int i = 0; i < bits; i++)
-                value |= (T)Convert.ChangeType(((bytes[offset / 8] & (1 << offset++ % 8)) != 0 ? 1UL : 0UL) << i, typeof(T));
+                value |= (T)Convert.ChangeType(((bytes[_offset / 8] & (1 << _offset++ % 8)) != 0 ? 1UL : 0UL) << i, typeof(T));
 
             return value;
         }
--- SimpleBitPacker/BitWriter.cs
+++ SimpleBitPacker/BitWriter.cs
@@ -2,23 +2,23 @@
 
 namespace SimpleBitPacker {
     public class BitWriter {
-        private List<byte> buffer = [0];
-        private int bitsInBuffer = 0;
+        private readonly List<byte> _buffer = [0];
+        private int _bitsInBuffer = 0;
 
         public void Write<T>(T value, int bits) where T : IBinaryInteger<T> {
             for (int i = 0; i < bits; i++) {
-                if (bitsInBuffer / 8 > buffer.Count - 1)
-                    buffer.Add(0);
+                if (_bitsInBuffer / 8 > _buffer.Count - 1)
+                    _buffer.Add(0);
 
                 if ((value & (T.One << i)) != T.Zero)
-                    buffer[buffer.Count - 1] |= (byte)(1 << bitsInBuffer % 8);
+                    _buffer[^1] |= (byte)(1 << _bitsInBuffer % 8);
 
-                bitsInBuffer++;
+                _bitsInBuffer++;
             }
         }
 
         public void Write(bool value) => Write(value ? 1 : 0, 1);
 
-        public byte[] GetBytes() => buffer.ToArray();
+        public byte[] GetBytes() => _buffer.ToArray();
     }
 }
